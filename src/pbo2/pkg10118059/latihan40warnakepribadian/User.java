e4c87193362315825ba86ad3bd116a752f2605dc
Muhammad Rakha Firdaus <48128093+mrakhaf@users.noreply.github.com>
2019-10-17 10:31:12 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118059/latihan40warnakepribadian/User.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118059.latihan40warnakepribadian;
+
+/**
+ *
+ * @author Hafizha Husnaisa
+ */
+public class User {
+    
+}
